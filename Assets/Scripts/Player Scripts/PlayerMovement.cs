df200f3bf07b2204cc7905956517488f8b4a1e45
Zachary Dowd <zacharyd2796@zohomail.com>
2021-12-28 09:50:34 -0700

    horizontal movement added

---
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed = 5f;
+
+    private Rigidbody2D myBody;
+    private Animator anim;
+
+    private void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        PlayerWalk();
+    }
+
+
+    void PlayerWalk()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+
+        if(h > 0)
+        {
+            myBody.velocity = new Vector2(speed, myBody.velocity.y);
+        } else if(h < 0)
+        {
+            myBody.velocity = new Vector2(-speed, myBody.velocity.y);
+        }
+    }
+
+} // class
