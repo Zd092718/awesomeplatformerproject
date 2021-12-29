84ab9690e78b0ec8c04e7a08a6147afd06a446ba
Zachary Dowd <zacharyd2796@zohomail.com>
2021-12-29 10:46:16 -0700

    snail movement in, working on collision

---
+++ Assets/Scripts/Enemy Scripts/SnailScript.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnailScript : MonoBehaviour
+{
+
+    public float moveSpeed = 1f;
+    private Rigidbody2D myBody;
+    private Animator anim;
+
+    private bool moveLeft;
+
+    private bool canMove;
+    private bool stunned;
+
+    
+    public Transform left_Collision, right_Collision, top_Collision, down_Collision;
+
+    private void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveLeft = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moveLeft)
+        {
+            myBody.velocity = new Vector2(-moveSpeed, myBody.velocity.y);
+        } else
+        {
+            myBody.velocity = new Vector2(moveSpeed, myBody.velocity.y);
+        }
+
+        CheckCollision();
+    }
+
+    void CheckCollision() 
+    {   
+        //if collision is not detected
+        if(!Physics2D.Raycast(down_Collision.position, Vector2.down, 0.1f))
+        {
+            ChangeDirection();
+        }
+    }
+
+    void ChangeDirection()
+    {
+        moveLeft = !moveLeft;
+        Vector3 tempScale = transform.localScale;
+
+        if (moveLeft)
+        {
+            tempScale.x = Mathf.Abs(tempScale.x);
+        } else
+        {
+            tempScale.x = -Mathf.Abs(tempScale.x);
+        }
+
+        transform.localScale = tempScale;
+    }
+
+
+}//class
