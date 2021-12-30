8ad4121b311dd8c7edc4cf01f5f01a3b4bf253e2
Zachary Dowd <zacharyd2796@zohomail.com>
2021-12-30 07:52:13 -0700

    snail colliders added and functioning

--- Assets/Scripts/Enemy Scripts/SnailScript.cs
+++ Assets/Scripts/Enemy Scripts/SnailScript.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SnailScript : MonoBehaviour
@@ -9,43 +7,114 @@ public class SnailScript : MonoBehaviour
     private Rigidbody2D myBody;
     private Animator anim;
 
+    public LayerMask playerLayer;
+
     private bool moveLeft;
 
     private bool canMove;
     private bool stunned;
 
-    
+
     public Transform left_Collision, right_Collision, top_Collision, down_Collision;
+    private Vector3 left_Collision_Position, right_Collision_Position;
 
     private void Awake()
     {
         myBody = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+
+        left_Collision_Position = left_Collision.position;
+        right_Collision_Position = right_Collision.position;
+
+
     }
     // Start is called before the first frame update
     void Start()
     {
         moveLeft = true;
+        canMove = true;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (moveLeft)
+        if (canMove)
         {
-            myBody.velocity = new Vector2(-moveSpeed, myBody.velocity.y);
-        } else
-        {
-            myBody.velocity = new Vector2(moveSpeed, myBody.velocity.y);
+            if (moveLeft)
+            {
+                myBody.velocity = new Vector2(-moveSpeed, myBody.velocity.y);
+            }
+            else
+            {
+                myBody.velocity = new Vector2(moveSpeed, myBody.velocity.y);
+            }
         }
+
 
         CheckCollision();
     }
 
-    void CheckCollision() 
-    {   
+    void CheckCollision()
+    {
+        RaycastHit2D leftHit = Physics2D.Raycast(left_Collision.position, Vector2.left, 0.1f, playerLayer);
+        RaycastHit2D rightHit = Physics2D.Raycast(right_Collision.position, Vector2.right, 0.1f, playerLayer);
+
+        Collider2D topHit = Physics2D.OverlapCircle(top_Collision.position, 0.2f, playerLayer);
+
+        if (topHit != null)
+        {
+            if (topHit.gameObject.tag == MyTags.PLAYER_TAG)
+            {
+                if (!stunned)
+                {
+                    //bounce after top hit
+                    topHit.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(topHit.gameObject.GetComponent<Rigidbody2D>().velocity.x, 7f);
+
+                    canMove = false;
+                    myBody.velocity = new Vector2(0, 0);
+                    anim.Play("SnailStunned");
+                    stunned = true;
+
+                    // Beetle code here
+
+                }
+            }
+        }
+
+        if (leftHit)
+        {
+            if (leftHit.collider.gameObject.tag == MyTags.PLAYER_TAG)
+            {
+                if (!stunned)
+                {
+                    //Apply Damage to player
+                    print("Damage left");
+                }
+                else
+                {
+                    myBody.velocity = new Vector2(15f, myBody.velocity.y);
+                }
+            }
+        }
+
+        if (rightHit)
+        {
+            if (rightHit.collider.gameObject.tag == MyTags.PLAYER_TAG)
+            {
+                if (!stunned)
+                {
+                    //apply damage to player
+                    print("damage right");
+                }
+                else
+                {
+                    myBody.velocity = new Vector2(-15f, myBody.velocity.y);
+                }
+            }
+        }
         //if collision is not detected
-        if(!Physics2D.Raycast(down_Collision.position, Vector2.down, 0.1f))
+        if (!Physics2D.Raycast(down_Collision.position, Vector2.down, 0.1f))
         {
             ChangeDirection();
         }
@@ -59,9 +128,16 @@ public class SnailScript : MonoBehaviour
         if (moveLeft)
         {
             tempScale.x = Mathf.Abs(tempScale.x);
-        } else
+
+            left_Collision.position = left_Collision_Position;
+            right_Collision.position = right_Collision_Position;
+
+        }
+        else
         {
             tempScale.x = -Mathf.Abs(tempScale.x);
+            left_Collision.position = right_Collision_Position;
+            right_Collision.position = left_Collision.position;
         }
 
         transform.localScale = tempScale;
